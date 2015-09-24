b9263809727994f175f11d797e322fc193c80d03
Richo <richo2192000@gmail.com>
2015-09-24 21:42:54 +0800

    Initial state of my personal blogs

---
+++ pelicanconf.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Richo'
+SITENAME = u'Note for Richo'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+# Specify a customized theme, via path relative to the settings file
+THEME = "themes/nest"
+# Add items to top menu before pages
+MENUITEMS = [('Homepage', '/'), ('Categories', '/categories.html')]
+
+# Footer
+NEST_SITEMAP_COLUMN_TITLE = u'Sitemap'
+NEST_SITEMAP_MENU = [
+    ('Archives', '/archives.html'),
+    ('Tags', '/tags.html'),
+    ('Authors', '/authors.html')
+]
+NEST_SITEMAP_ATOM_LINK = u'Atom Feed'
+NEST_SITEMAP_RSS_LINK = u'RSS Feed'
+NEST_SOCIAL_COLUMN_TITLE = u'Social'
+SOCIAL = [
+    ('Github', 'https://github.com/richohan')
+]
+NEST_LINKS_COLUMN_TITLE = u'Links'
+NEST_COPYRIGHT = u'&copy; Richo 2015'
+
+# index.html
+NEST_INDEX_HEAD_TITLE = u'Homepage'
+NEST_INDEX_HEADER_TITLE = u'Note for Richo'
+NEST_INDEX_HEADER_SUBTITLE = u'Journal for coding stuff'
+NEST_INDEX_CONTENT_TITLE = u'Last Updates'
+
+# archives.html
+NEST_ARCHIVES_HEAD_TITLE = u'Archives'
+NEST_ARCHIVES_HEAD_DESCRIPTION = u'Posts Archives'
+NEST_ARCHIVES_HEADER_TITLE = u'Archives'
+NEST_ARCHIVES_HEADER_SUBTITLE = u'Archives for all posts'
+NEST_ARCHIVES_CONTENT_TITLE = u'Archives'
+
+# authors.html
+NEST_AUTHORS_HEAD_TITLE = u'Author list'
+NEST_AUTHORS_HEAD_DESCRIPTION = u'Author list'
+NEST_AUTHORS_HEADER_TITLE = u'Author list'
+NEST_AUTHORS_HEADER_SUBTITLE = u'Archives listed by author'
+
+# categories.html
+NEST_CATEGORIES_HEAD_TITLE = u'Categories'
+NEST_CATEGORIES_HEAD_DESCRIPTION = u'Archives listed by category'
+NEST_CATEGORIES_HEADER_TITLE = u'Categories'
+NEST_CATEGORIES_HEADER_SUBTITLE = u'Archives listed by category'
+
+# pagination.html
+NEST_PAGINATION_PREVIOUS = u'Previous'
+NEST_PAGINATION_NEXT = u'Next'
+
+# tags.html
+NEST_TAGS_HEAD_TITLE = u'Tags'
+NEST_TAGS_HEAD_DESCRIPTION = u'Tags List'
+NEST_TAGS_HEADER_TITLE = u'Tags'
+NEST_TAGS_HEADER_SUBTITLE = u'Tags List'
+NEST_TAGS_CONTENT_TITLE = u'Tags List'
+NEST_TAGS_CONTENT_LIST = u'tagged'
